5e18b9feacad7e5ce665372dad0265b482b0faa3
Quynh <bachcongtu2904@gmail.com>
2020-11-11 08:31:22 +0700

    update

--- logn-in.php
+++ logn-in.php
@@ -1,5 +1,5 @@
 <body style="background: aquamarine">
-<form method="post" action="https://github.com/Le-Quynh-29/php-logn-in.git" style="position: absolute; left: 40%;background: azure;top:100px; border: 1px solid black; width: 300px; height: 200px">
+<form method="post" style="position: absolute; left: 40%;background: azure;top:100px; border: 1px solid black; width: 300px; height: 200px">
     <h1>Đăng nhập</h1>
     <input type="text" name="User" placeholder="Tài khoản"> <br><br>
     <input type="password" name="Password" placeholder="Mật khẩu"> <br><br>
@@ -9,20 +9,27 @@
 <?php
 $arr = array(["admin","admin"],["quynh",'123456']);
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $user = $_POST["User"];
-    $pass = $_POST["Password"];
-    $login = false;
-    foreach ($arr as $value) {
-            if($user == $arr[0][0] && $pass == $arr[0][1]){
+
+    if (!empty($_POST['User']) && (!empty($_POST['Password']))) {
+        $user = $_POST["User"];
+        $pass = $_POST["Password"];
+        $login = false;
+        foreach ($arr as $value) {
+            if ($user == $arr[0][0] && $pass == $arr[0][1]) {
+                header("location: https://github.com/Le-Quynh-29/php-logn-in.git");
                 $login = true;
                 break;
             }
+
         }
-    if ($login) {
-        echo "dang nhap thanh cong";
+        if ($login) {
+            echo "dang nhap thanh cong";
+        } else {
+            echo "dang nhap sai";
+        }
     }
     else{
-        echo "dang nhap sai";
+        echo " ban chua nhap gi";
     }
 }
 ?>
